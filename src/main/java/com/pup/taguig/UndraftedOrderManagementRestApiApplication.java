5e461308cca94abdca425d34d9f74ca73e7f9f30
Adriel Joseph Dimayuga <dimayuga.adrieljoseph03@gmail.com>
2026-06-13 18:33:44 +0800

    first commit

---
+++ src/main/java/com/pup/taguig/UndraftedOrderManagementRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.pup.taguig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndraftedOrderManagementRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndraftedOrderManagementRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pup/taguig/UndraftedOrderManagementRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pup.taguig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndraftedOrderManagementRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
