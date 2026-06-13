1206f3a7662550027005186b4bd9ce7e819c4417
Adriel Joseph Dimayuga <dimayuga.adrieljoseph03@gmail.com>
2026-06-13 21:36:25 +0800

    add customer get and post

--- src/main/java/com/pup/taguig/UndraftedOrderManagementRestApiApplication.java
+++ src/main/java/com/pup/taguig/UndraftedOrderManagementRestApiApplication.java
@@ -1,9 +1,11 @@
 package com.pup.taguig;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.pup.taguig.repository")
 public class UndraftedOrderManagementRestApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(UndraftedOrderManagementRestApiApplication.class, args);
